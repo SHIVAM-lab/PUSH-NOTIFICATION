3a86cc8932fd05ffb71205ec1e4d59caa22d414f
SHIVAM-lab <56170689+SHIVAM-lab@users.noreply.github.com>
2021-06-13 17:03:48 +0530

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-var app = require('./routes/index');
-require('mongoose').connect('mongodb://localhost/tattletale' ,{useNewUrlParser: true,
-useUnifiedTopology: true});
-require('dotenv').config();
-var port = 3000;
-app.listen(port, console.log(`Server is running on ${port}`));
